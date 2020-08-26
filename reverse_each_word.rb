12af3ab0767f653436acfb330956b22e2766492a
Nasrin Rahimi <77.nasrin@gmail.com>
2020-08-26 19:10:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-def reverse_each_word(sentence)
-  array = sentence.split(" ")
-  reversed_array = []
-  array.each do |word|
-    reversed_array << word.reverse
-  end
-  reversed_sentence =  reversed_array.join(" ")
-end
+# def reverse_each_word(sentence)
+#   array = sentence.split(" ")
+#   reversed_array = []
+#   array.each do |word|
+#     reversed_array << word.reverse
+#   end
+#   reversed_sentence =  reversed_array.join(" ")
+# end
 
 def reverse_each_word(sentence)
   array = sentence.split(" ")
